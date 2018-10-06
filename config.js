3bd048b51bfa730718869e6d73ef7957f1569574
Prashant Shekher <prashant.shekher@wishfin.com>
2018-10-06 17:27:59 +0530

    Set Proper Access Token

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     WA_USER_NAME: "admin",
     WA_PASSWORD_NAME: "Welcome!1",
     //Dialogflow ACCESS TOEKN  
-    APIAI_CLIENT_ACCESS_TOKEN: "1187853034b8400d99d3ebca1e690bf0",
+    APIAI_CLIENT_ACCESS_TOKEN: "7112d644ff7c42f6bde91c8955507bce",
 };
 
